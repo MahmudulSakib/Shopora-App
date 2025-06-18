7041fb305f7e7d5acc31a1643f5c5922eeee4b67
Mahmudul H. Sakib <mahmudulsakib.110@gmail.com>
2025-06-18 22:08:37 +0600

    Update server.ts

--- admin-panel/backend/server.ts
+++ admin-panel/backend/server.ts
@@ -40,7 +40,7 @@ const port = 8080;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001"], // ✅ allow both
+    origin: ["http://localhost:3000", "http://localhost:3001"], 
     credentials: true,
   },
 });
